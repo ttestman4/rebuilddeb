010bebfdd73c77876e23f4d1b73ff4bc5465c958
Test Man <ttestman4@gmail.com>
2019-02-22 00:20:48 +0530

    sass dependency

--- rebuild.sh
+++ rebuild.sh
@@ -9,7 +9,8 @@
 #sudo apt-get install -y nodejs
 #node --version
 #npm --version
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt-get update && sudo apt-get install yarn
-yarn --version
+#curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+#echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+#sudo apt-get update && sudo apt-get install yarn
+#yarn --version
+sudo apt-get install build-essential
