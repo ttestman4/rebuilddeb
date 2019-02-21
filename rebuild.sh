ecb5bc1249e653b62bc49fa086078bc975c4479f
Test Man <ttestman4@gmail.com>
2019-02-21 21:39:25 +0530

    initial commit

---
+++ rebuild.sh
@@ -0,0 +1,6 @@
+#sudo apt-get install git
+#sudo add-apt-repository ppa:git-core/ppa 
+#sudo apt update
+#sudo apt install git
+git config --global user.name "Test Man"
+git config --global user.email ttestman4@gmail.com
