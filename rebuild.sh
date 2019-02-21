dd3fde1e5beb11268b3ac1198d5dec9953bd5ac2
Test Man <ttestman4@gmail.com>
2019-02-21 22:10:24 +0530

    update2

--- rebuild.sh
+++ rebuild.sh
@@ -1,6 +1,15 @@
-#sudo apt-get install git
 #sudo add-apt-repository ppa:git-core/ppa 
 #sudo apt update
 #sudo apt install git
-git config --global user.name "Test Man"
-git config --global user.email ttestman4@gmail.com
+#git config --global user.name "Test Man"
+#git config --global user.email ttestman4@gmail.com
+#git --version
+#sudo apt-get install curl
+#curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+#sudo apt-get install -y nodejs
+#node --version
+#npm --version
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get update && sudo apt-get install yarn
+yarn --version
